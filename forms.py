8464461c7480c2c0a0fc64d90e36ab01e8da3358
CodeByMF <ourcreatefactory@gmail.com>
2020-01-04 06:12:46 -0500

    Updated with additional functionality and added DB

--- forms.py
+++ forms.py
@@ -1,4 +1,5 @@
 from flask_wtf import FlaskForm
+from flask_wtf.file import FileField, FileAllowed
 from wtforms import StringField, SubmitField, DecimalField, DateField, SelectField
 from wtforms.fields.html5 import DateField
 from wtforms.validators import DataRequired
@@ -11,18 +12,27 @@ class ResourcesForm(FlaskForm):
     department = StringField('Department',
                         validators=[DataRequired()])
     allocation_percentage = StringField('Allocation %', validators=[DataRequired()])
+    marker_image = FileField('Add Marker Image', validators=[FileAllowed(['jpg','png'])])
     submit = SubmitField('Save Resource')
 
 
 class TaskForm(FlaskForm):
     project_title = StringField('Project Title',
                         validators=[DataRequired()])
-    task_id = DecimalField('Task ID', validators=[DataRequired()])
+    parent_id = DecimalField('Parent ID', validators=[DataRequired()])
     status = SelectField('Status', choices=[('NS','Not Started'), ('IP','In Progress'), ('DF', 'Deferred'), ('CP','Completed')])
     task_description= StringField('Task Description',
                         validators=[DataRequired()])
     start_date = DateField('Start Date', default=date.today)
     end_date = DateField('End Date', default=date.today)
     # add code to populate dropdown list with full names from db
-    resource = SelectField('Resource', choices=[('TBD','Not Selected'), ('MF','Martie Feldtmann'), ('MM', 'Mickey Mouse'), ('DD','Donald Duck')])                   
+    resource = SelectField('Resource', choices=[('TBD','0'), ('MF','1'), ('MM', '2'), ('DD','3')])                   
     submit = SubmitField('Save Task')
+
+class UpdateResourceForm(FlaskForm):
+    fullname = StringField('Fullname',
+                           validators=[DataRequired()])
+    department = StringField('Department',
+                        validators=[DataRequired()])
+    allocation_percentage = StringField('Allocation %', validators=[DataRequired()])
+    submit = SubmitField('Update Resource')
--- pmapp.py
+++ pmapp.py
@@ -1,37 +1,38 @@
+import os
 from flask import Flask, render_template, url_for, flash, redirect
-from forms import ResourcesForm, TaskForm
+from flask_sqlalchemy import SQLAlchemy
+from forms import ResourcesForm, TaskForm, UpdateResourceForm
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '5791628bb0b13ce0c676dfde280ba245'
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///pmappdb.db'
+
+db = SQLAlchemy(app)
+
+# to import the db - termnal -> from pmapp import db
+class Resource(db.Model):
+    resource_id = db.Column(db.Integer, primary_key=True)
+    fullname = db.Column(db.String(150), nullable=False)
+    department = db.Column(db.String(100), nullable=False) 
+    allocation_percentage = db.Column(db.Integer, nullable=False)
+    marker_image = db.Column(db.String(20), nullable=False, default='default_marker.jpg')
+    tasks = db.relationship('Task', backref='resource', lazy=True)
+
+
+class Task(db.Model):
+    task_id = db.Column(db.Integer, primary_key=True)
+    resource_id = db.Column(db.Integer, db.ForeignKey('resource.resource_id'), nullable=False)
+    project_title = db.Column(db.String(150), nullable=False)
+    parent_id = db.Column(db.Float, nullable=False)
+    status = db.Column(db.String(15), nullable=False)
+    task_description = db.Column(db.String(200), nullable=False) 
+    start_date = db.Column(db.DateTime, nullable=False)
+    end_date = db.Column(db.DateTime, nullable=False)
+    marker_image = db.Column(db.String(20), nullable=False, default='default_marker.jpg')
+
+
 # dummy data
-task = [
-    {
-        'project_title': 'Test Project',
-        'task_id': 1,
-        'status': 'In Progress',
-        'task_description': 'Requirements Definition (Phase 1)',
-        'start_date': '11/30/2019',
-        'end_date': '03/31/2020',
-        'resource': 'Martie Feldtmann'
-    },
-    {
-        'project_title': 'Test Project',
-        'task_id': 1.01,
-        'status': 'In Progress',
-        'task_description': 'Requirements funding',
-        'start_date': '12/01/2019',
-        'end_date': '12/31/2019',
-        'resource': 'Bob Iger'
-    },
-    {
-        'project_title': 'Test Project',
-        'task_id': 1.0101,
-        'status': 'In Progress',
-        'task_description': 'Review project request',
-        'start_date': '12/01/2019',
-        'end_date': '12/31/2019',
-        'resource': 'Bob Iger'
-    }
-]
+task=[]
 
 
 @app.route("/")
@@ -39,11 +40,22 @@ task = [
 def home():
     return render_template('home.html')
 
+def save_marker_image(form_picture):
+    marker_file = form_picture.filename
+    marker_path = os.path.join(app.root_path, 'static/marker_pics', marker_file)
+    form_picture.save(marker_path)
+    return marker_path
+
 @app.route("/resources", methods=['GET', 'POST'])
 def resources():
     form = ResourcesForm()
     if form.validate_on_submit():
+        if form.marker_image.data:
+            marker_image_file = save_marker_image(form.marker_image.data)
         flash(f'Resource {form.fullname.data} saved!', 'success')
+        resource = Resource(fullname=form.fullname.data, department=form.department.data, allocation_percentage=form.allocation_percentage.data, marker_image=marker_image_file)
+        db.session.add(resource)
+        db.session.commit()
     return render_template('resources.html', title='Resources', form=form) 
 
 @app.route("/tasks", methods=['GET', 'POST'])
@@ -51,7 +63,10 @@ def tasks():
     form = TaskForm()
     if form.validate_on_submit():
         flash(f'Resource {form.task_description.data} saved!', 'success')
-    return render_template('tasks.html', title='Tasks', form=form, posts=task) #passing the dummy data to the web page
+        task = Task(project_title=form.project_title.data, parent_id=form.parent_id.data, status=form.status.data, task_description=form.task_description.data, start_date=form.start_date.data, end_date=form.end_date.data, resource_id=form.resource.data)
+        db.session.add(task)
+        db.session.commit()
+    return render_template('tasks.html', title='Tasks', form=form ) #passing the dummy data to the web page - removed posts=task
 
 @app.route("/about")
 def about():
